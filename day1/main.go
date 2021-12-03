a01540aef814238ac936d6f0d9b82b039b77a28a
xartishuk <xartishuk@gmail.com>
2021-12-03 13:35:05 +0300

    day1: added test

--- day1/main.go
+++ day1/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	input, err := readInput("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result := SonarSweep(input)
+
+	fmt.Println(result)
+}
+
+func SonarSweep([]int) int {
+	return 0
+}
+
+func readInput(filename string) ([]int, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	res := make([]int, 0)
+
+	s := bufio.NewScanner(f)
+
+	for s.Scan() {
+		num, _ := strconv.Atoi(s.Text())
+		res = append(res, num)
+	}
+
+	return res, s.Err()
 }
--- day1/main_test.go
+++ day1/main_test.go
@@ -2,5 +2,25 @@ package main
 
 import "testing"
 
-func TestFoo(t *testing.T) {
+func TestSonarSweep(t *testing.T) {
+	tests := []struct {
+		inputFile string
+		expected  int
+	}{{
+		inputFile: "input_test.txt",
+		expected:  7,
+	}}
+
+	for _, tt := range tests {
+		input, err := readInput(tt.inputFile)
+		if err != nil {
+			t.Error(err)
+		}
+
+		result := SonarSweep(input)
+
+		if result != tt.expected {
+			t.Errorf("expected %d, got %d", tt.expected, result)
+		}
+	}
 }
