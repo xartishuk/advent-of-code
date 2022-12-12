153e1d2bd8c58be99adaa70470346c2b7ab90dbf
Viktar Tsishuk <c-Viktar.Tsishuk@integrate.com>
2022-12-12 15:34:07 +0400

    2022/10: add part 2

--- 2022/day10/main.go
+++ 2022/day10/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"strconv"
 )
 
 func main() {
-	in, err := readInput("input_test.txt")
+	in, err := readInput("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -17,13 +18,15 @@ func main() {
 	fmt.Println(visited)
 }
 
+const screenWidth = 40
+
 func CRTCPU(instructions []string) int {
 	var sum int
 
-	cpu := newCPU(instructions)
+	cpu := newCPU(instructions, os.Stdout)
 
-	for cpu.LoadOp() {
-		if (cpu.Cycle()-20)%40 == 0 {
+	for cpu.Draw() {
+		if (cpu.Cycle()-20)%screenWidth == 0 {
 			sum += cpu.SignalStrength()
 		}
 
@@ -36,19 +39,34 @@ func CRTCPU(instructions []string) int {
 type CPU struct {
 	x int
 
+	// instruction pointer doubles as crt beam position
 	ip int
 
 	instructions []string
+
+	screen *os.File
 }
 
-func newCPU(instructions []string) *CPU {
+func newCPU(instructions []string, screen *os.File) *CPU {
 	return &CPU{
 		x:            1,
 		instructions: instructions,
+		screen:       screen,
 	}
 }
 
-func (c *CPU) LoadOp() bool {
+func (c *CPU) Draw() bool {
+	// sprite intersects CRT beam
+	if c.x-1 <= c.beam() && c.beam() <= c.x+1 {
+		c.screen.WriteString("#")
+	} else {
+		c.screen.WriteString(".")
+	}
+
+	if (c.ip+1)%screenWidth == 0 {
+		c.screen.WriteString("\n")
+	}
+
 	return c.ip < len(c.instructions)
 }
 
@@ -67,4 +85,8 @@ func (c *CPU) Cycle() int {
 
 func (c *CPU) SignalStrength() int {
 	return c.Cycle() * c.x
+}
+
+func (c *CPU) beam() int {
+	return c.ip % screenWidth
 }
