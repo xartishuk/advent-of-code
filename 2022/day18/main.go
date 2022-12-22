94a24dc933ffd1347f1bac58f781a80012b88405
Viktar Tsishuk <c-Viktar.Tsishuk@integrate.com>
2022-12-22 19:15:14 +0400

    2022/18: add part 2

--- 2022/day18/main.go
+++ 2022/day18/main.go
@@ -3,20 +3,21 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 )
 
 func main() {
-	in, err := readInput("input_test.txt")
+	in, err := readInput("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	result := SurfaceArea(in)
+	result := ExternalSurfaceArea(in)
 
 	fmt.Println(result)
 }
 
-func SurfaceArea(drops []Drop) int {
+func TotalSurfaceArea(drops []Drop) int {
 	for i := 0; i < len(drops); i++ {
 		for j := i + 1; j < len(drops); j++ {
 			if drops[i].Sides == 0 {
@@ -39,6 +40,146 @@ func SurfaceArea(drops []Drop) int {
 
 	return area
 }
+
+func ExternalSurfaceArea(drops []Drop) int {
+	areaCounter := newExternalAreaCounter(drops)
+
+	return areaCounter.CountFromStart()
+}
+
+func newExternalAreaCounter(drops []Drop) *ExternalAreaCounter {
+	bounds := newBounds(drops)
+
+	rocks := map[int]struct{}{}
+
+	for _, d := range drops {
+		rocks[bounds.I(d.x, d.y, d.z)] = struct{}{}
+	}
+
+	return &ExternalAreaCounter{
+		bounds: bounds,
+		rocks:  rocks,
+		water:  map[int]struct{}{},
+	}
+}
+
+type ExternalAreaCounter struct {
+	bounds Bounds
+	rocks  map[int]struct{}
+	water  map[int]struct{}
+
+	area int
+}
+
+func (c *ExternalAreaCounter) CountFromStart() int {
+	c.count(c.bounds.minX, c.bounds.minY, c.bounds.minZ)
+
+	return c.area
+}
+
+func (c *ExternalAreaCounter) count(x, y, z int) {
+	c.water[c.bounds.I(x, y, z)] = struct{}{}
+
+	if c.bounds.InX(x + 1) {
+		i := c.bounds.I(x+1, y, z)
+		if _, ok := c.rocks[i]; ok {
+			c.area++
+		} else if _, ok := c.water[i]; !ok {
+			c.count(x+1, y, z)
+		}
+	}
+	if c.bounds.InX(x - 1) {
+		i := c.bounds.I(x-1, y, z)
+		if _, ok := c.rocks[i]; ok {
+			c.area++
+		} else if _, ok := c.water[i]; !ok {
+			c.count(x-1, y, z)
+		}
+	}
+	if c.bounds.InY(y + 1) {
+		i := c.bounds.I(x, y+1, z)
+		if _, ok := c.rocks[i]; ok {
+			c.area++
+		} else if _, ok := c.water[i]; !ok {
+			c.count(x, y+1, z)
+		}
+	}
+	if c.bounds.InY(y - 1) {
+		i := c.bounds.I(x, y-1, z)
+		if _, ok := c.rocks[i]; ok {
+			c.area++
+		} else if _, ok := c.water[i]; !ok {
+			c.count(x, y-1, z)
+		}
+	}
+	if c.bounds.InZ(z + 1) {
+		i := c.bounds.I(x, y, z+1)
+		if _, ok := c.rocks[i]; ok {
+			c.area++
+		} else if _, ok := c.water[i]; !ok {
+			c.count(x, y, z+1)
+		}
+	}
+	if c.bounds.InZ(z - 1) {
+		i := c.bounds.I(x, y, z-1)
+		if _, ok := c.rocks[i]; ok {
+			c.area++
+		} else if _, ok := c.water[i]; !ok {
+			c.count(x, y, z-1)
+		}
+	}
+}
+
+func newBounds(drops []Drop) Bounds {
+	minX, minY, minZ := math.MaxInt, math.MaxInt, math.MaxInt
+	var maxX, maxY, maxZ int
+
+	for _, d := range drops {
+		if d.x > maxX {
+			maxX = d.x
+		}
+		if d.y > maxY {
+			maxY = d.y
+		}
+		if d.z > maxZ {
+			maxZ = d.z
+		}
+		if d.x < minX {
+			minX = d.x
+		}
+		if d.y < minY {
+			minY = d.y
+		}
+		if d.z < minZ {
+			minZ = d.z
+		}
+	}
+
+	return Bounds{
+		minX:  minX - 1,
+		minY:  minY - 1,
+		minZ:  minZ - 1,
+		maxX:  maxX + 1,
+		maxY:  maxY + 1,
+		maxZ:  maxZ + 1,
+		sizeX: maxX - minX + 3,
+		sizeY: maxY - minY + 3,
+		sizeZ: maxZ - minZ + 3,
+	}
+}
+
+type Bounds struct {
+	minX, minY, minZ, maxX, maxY, maxZ int
+	sizeX, sizeY, sizeZ                int
+}
+
+func (b Bounds) I(x, y, z int) int {
+	return x*b.sizeY*b.sizeZ + y*b.sizeZ + z
+}
+
+func (b Bounds) InX(x int) bool { return b.minX <= x && x <= b.maxX }
+func (b Bounds) InY(y int) bool { return b.minY <= y && y <= b.maxY }
+func (b Bounds) InZ(z int) bool { return b.minZ <= z && z <= b.maxZ }
 
 type Drop struct {
 	x, y, z int
--- 2022/day18/main_test.go
+++ 2022/day18/main_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestSurfaceArea(t *testing.T) {
+func TestTotalSurfaceArea(t *testing.T) {
 	tests := []struct {
 		inputFile string
 		result    int
@@ -23,7 +23,36 @@ func TestSurfaceArea(t *testing.T) {
 			t.Error(err)
 		}
 
-		result := SurfaceArea(in)
+		result := TotalSurfaceArea(in)
+
+		if result != tt.result {
+			t.Errorf("expected %d, got %d", tt.result, result)
+		}
+	}
+}
+
+func TestExternalSurfaceArea(t *testing.T) {
+	tests := []struct {
+		inputFile string
+		result    int
+	}{
+		{
+			inputFile: "input_test.txt",
+			result:    58,
+		},
+		{
+			inputFile: "input.txt",
+			result:    2530,
+		},
+	}
+
+	for _, tt := range tests {
+		in, err := readInput(tt.inputFile)
+		if err != nil {
+			t.Error(err)
+		}
+
+		result := ExternalSurfaceArea(in)
 
 		if result != tt.result {
 			t.Errorf("expected %d, got %d", tt.result, result)
